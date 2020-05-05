679d4a6310519459747da8c9c49fd31a4d2d0c38
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-05 22:57:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,3 @@
 def line(array)
-  array.length > 0 ? array.each {|spot| "#{array.index(spot) +1}. #{spot}"} : "The line is currently empty."
+  array.length > 0 ? array.each {|spot| puts"#{array.index(spot) +1}. #{spot}"} : puts"The line is currently empty."
 end
